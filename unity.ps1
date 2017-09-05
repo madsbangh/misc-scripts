ab09f2b56458a421f5da3ea5eba3eb8bc7c85ff7
Mads Bang Hoffensetz <faetter.mads@hotmail.com>
2017-09-05 21:01:51 +0200

    Update unity.ps1

--- unity.ps1
+++ unity.ps1
@@ -1,1 +1,11 @@
-& 'C:\Program Files\Unity_2017.1\Editor\Unity.exe' -projectPath (get-location) > $null
+$UnityPath = 'C:\Program Files\Unity_2017.1\Editor\Unity.exe'
+
+# Is this a unity project?
+if (Test-Path ./Assets)
+{
+    # Set project path to current dir
+    $args += '-projectPath (get-location)'
+}
+
+# Start unity with args
+Invoke-Expression "& '$UnityPath' $args" > $null
