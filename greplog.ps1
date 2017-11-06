ef5d335231d1e1faa0da4d4b7afb983a5fef7f94
Mads Bang Hoffensetz <faetter.mads@hotmail.com>
2017-11-06 19:06:01 +0100

    Fix greplog

--- greplog.ps1
+++ greplog.ps1
@@ -1,1 +1,1 @@
-git shortlog --no-merges --grep "$args[0]" --regexp-ignore-case --extended-regexp --pretty=format:"%ar: %s%n"
+git shortlog --no-merges --grep $args[0] --regexp-ignore-case --extended-regexp --pretty=format:"%ar: %s%n"
