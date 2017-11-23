1165be4fa948684ff877117f8d77017c5d350bba
Mads Bang Hoffensetz <faetter.mads@hotmail.com>
2017-11-23 23:39:37 +0100

    Make the recent changes script not print merges

--- recent.ps1
+++ recent.ps1
@@ -1,1 +1,1 @@
-﻿git log --format="%aN" | sort -u | foreach {git log --format="%aN (%ar): %s" --author=$_ --since="1 day ago"; echo ""}
+﻿git log --format="%aN" | sort -u | foreach {git log --format="%aN (%ar): %s" --author=$_ --since="1 day ago" --no-merges; echo ""}
