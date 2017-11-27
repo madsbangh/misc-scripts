b73169b092dcf6a885a43ac1230616b4261bc3f1
Mads Bang Hoffensetz <faetter.mads@hotmail.com>
2017-11-27 10:28:16 +0100

    Make recent changes script more flexible

--- recent.ps1
+++ recent.ps1
@@ -1,1 +1,18 @@
-﻿git log --format="%aN" | sort -u | foreach {git log --format="%aN (%ar): %s" --author=$_ --since="1 day ago" --no-merges; echo ""}
+﻿param
+(
+    [switch] $hash,
+    [switch] $time
+)
+if ($time)
+{
+    $format = "%aN (%ad): %s"
+}
+else
+{
+    $format = "%aN (%ar): %s"
+}
+if ($hash)
+{
+    $format += " (%h)"
+}
+git log --format="%aN" | sort -u | foreach {git log --format="$format" --author=$_ --since="1 day ago" --no-merges; echo ""}
