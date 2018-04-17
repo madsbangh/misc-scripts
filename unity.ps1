d1d1b82fedf3b342d9c76a4928c0a9598d1623aa
madsbangh <faetter.mads@hotmail.com>
2018-04-17 02:04:29 +0200

    Update unity launch script

--- unity.ps1
+++ unity.ps1
@@ -1,10 +1,21 @@
-$UnityPath = 'C:\Program Files\Unity_2017.3\Editor\Unity.exe'
+${Unity_2017.3} = 'C:\Program Files\Unity_2017.3\Editor\Unity.exe'
+${Unity_5.6.3p1} = 'C:\Program Files\Unity_5.6.3p1\Editor\Unity.exe'
+
+# Default Unity Path
+$UnityPath = ${Unity_2017.3}
 
 # Is this a unity project?
 if (Test-Path ./Assets)
 {
     # Set project path to current dir
     $args += '-projectPath (get-location)'
+
+    # Test Unity version
+    if ((Get-Content "$(Get-Location)\ProjectSettings\ProjectVersion.txt") -like '*5.6.3p1')
+    {
+        echo "Project is using Unity 5.6.3p1"
+        $UnityPath = ${Unity_5.6.3p1}
+    }
 }
 
 # Start unity with args
