a84a85551085b99b9f938bd8a0d975dced3f8925
Mads Bang Hoffensetz <faetter.mads@hotmail.com>
2019-03-08 10:55:54 +0100

    Update adbstart.ps1

--- adbstart.ps1
+++ adbstart.ps1
@@ -4,3 +4,4 @@ if ($pacname -eq $null)
     $pacname = (Get-PackageName)
 }
 adb shell monkey -p $pacname -c android.intent.category.LAUNCHER 1
+# adb shell am start -n "$pacname/com.unity3d.player.UnityPlayerActivity"
