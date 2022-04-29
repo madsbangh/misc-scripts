293483ada5d1389db48d34e92516a687df547f19
Mads <mbh@kanda.dk>
2022-04-29 09:40:06 +0200

    Add pythin to ignore script

--- ignore.ps1
+++ ignore.ps1
@@ -1,7 +1,7 @@
 param
 (
     [Parameter(Position=0, Mandatory=$True)]
-    [ValidateSet("Unity", IgnoreCase=$False)]
+    [ValidateSet("Unity", "Python", IgnoreCase=$False)]
     [String]
     $Template
 )
